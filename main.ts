a790bacd72c660205d2c4e7b40a45fa3d1abff1e
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-01-16 16:52:53 -0600

    steps toward capture commands.

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 function wForward () {
     wcoord(WMove)
-    if (95 != led.pointBrightness(wx, wy - 2)) {
-        bright = 255
-        led.unplot(wx, wy)
-        basic.pause(500)
-        led.plotBrightness(wx, wy - 2, bright)
-        WHITE[WMove * 2 + 1] = wy - 2
-        wcoord(WMove)
+    canWCap(WMove)
+    if (canWCap(WMove) == 1) {
+        basic.showString("need to capture!")
     } else {
-        wBlink(WMove)
+        if (95 != led.pointBrightness(wx, wy - 2)) {
+            // record w move
+            // 
+            wmoves.unshift(WMove)
+            bright = 255
+            led.unplot(wx, wy)
+            basic.pause(500)
+            led.plotBrightness(wx, wy - 2, bright)
+            WHITE[WMove * 2 + 1] = wy - 2
+            wcoord(WMove)
+        } else {
+            wBlink(WMove)
+        }
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -18,7 +26,6 @@ input.onButtonPressed(Button.A, function () {
     }
     wcoord(WMove)
     wBlink(WMove)
-    wmoves.unshift(WMove)
 })
 function BBlink (num: number) {
     bcoord(num)
@@ -27,9 +34,28 @@ function BBlink (num: number) {
     basic.pause(500)
     led.plotBrightness(bx, by, bright)
 }
+function wCap (num: number) {
+    if (led.pointBrightness(wx + 2, wy - 2) == 95) {
+        return 1
+    }
+    if (led.pointBrightness(wx - 2, wy - 2) == 95) {
+        return 1
+    }
+    return 0
+}
 function bcoord (num: number) {
     bx = BLACK[num * 2]
     by = BLACK[num * 2 + 1]
+}
+function canWCap (num: number) {
+    wcoord(num)
+    if (led.pointBrightness(wx + 2, wy - 2) == 95) {
+        return 1
+    }
+    if (led.pointBrightness(wx - 2, wy - 2) == 95) {
+        return 1
+    }
+    return 0
 }
 input.onButtonPressed(Button.B, function () {
     wForward()
@@ -38,6 +64,16 @@ input.onButtonPressed(Button.B, function () {
     bForward()
     bmoves.unshift(BMove)
 })
+function canBCap (num: number) {
+    bcoord(num)
+    if (led.pointBrightness(bx + 2, by + 2) == 255) {
+        return 1
+    }
+    if (led.pointBrightness(bx - 2, by + 2) == 255) {
+        return 1
+    }
+    return 0
+}
 function wcoord (num: number) {
     wx = WHITE[num * 2]
     wy = WHITE[num * 2 + 1]
@@ -84,18 +120,6 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
-for (let index = 0; index <= 4; index++) {
-    led.plotBrightness(index, 1, 20)
-}
-for (let index2 = 0; index2 <= 4; index2++) {
-    led.plotBrightness(index2, 3, 20)
-}
-for (let index3 = 0; index3 <= 4; index3++) {
-    led.plotBrightness(1, index3, 20)
-}
-for (let index4 = 0; index4 <= 4; index4++) {
-    led.plotBrightness(3, index4, 20)
-}
 bmoves = []
 wmoves = []
 led.plotBrightness(0, 4, 255)
@@ -107,5 +131,5 @@ led.plotBrightness(0, 0, 95)
 WMove = 0
 BMove = 0
 WHITE = [0, 4, 2, 4, 4, 4]
-BLACK = [0, 0, 2, 0, 0, 0]
+BLACK = [0, 0, 2, 0, 4, 0]
 wBlink(0)
